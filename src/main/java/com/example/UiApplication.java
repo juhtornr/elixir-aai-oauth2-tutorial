9fceb3943dab7821a910ff3c9bc81a7fce3d11fd
Juha Törnroos <juha.tornroos@csc.fi>
2017-01-24 12:02:22 +0100

    Add /user endpoint for fetching information about authenticated user

--- src/main/java/com/example/UiApplication.java
+++ src/main/java/com/example/UiApplication.java
@@ -1,12 +1,19 @@
 package com.example;
 
+import java.security.Principal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableOAuth2Sso
 public class UiApplication {
+
+  	@RequestMapping("/user")
+  	public Principal user(Principal principal) {
+		return principal;
+   	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
