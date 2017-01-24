80e742e8da9995ef2f62ea21ac322f9b7bb9996b
Juha Törnroos <juha.tornroos@csc.fi>
2017-01-24 12:25:14 +0100

    Deny access to /user endpoint and permit all others

--- src/main/java/com/example/UiApplication.java
+++ src/main/java/com/example/UiApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class UiApplication {
+public class UiApplication extends WebSecurityConfigurerAdapter {
 
   	@RequestMapping("/user")
   	public Principal user(Principal principal) {
@@ -18,4 +20,15 @@ public class UiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
 	}
+
+  	@Override
+  	protected void configure(HttpSecurity http) throws Exception {
+	    	http
+		  .antMatcher("/**")
+		  .authorizeRequests()
+		    .antMatchers("/", "/login**", "/webjars/**")
+		    .permitAll()
+	          .anyRequest()
+          	    .authenticated();
+  	}	    
 }
